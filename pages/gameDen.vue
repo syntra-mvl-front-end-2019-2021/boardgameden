412ef5a038435944e139014e9121376d695f504a
stephane_mahop <tatosteph@gmail.com>
2021-04-24 20:21:54 +0200

    init

--- pages/gameDen.vue
+++ pages/gameDen.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 export default {
-  name: 'gameDen',
+  name: 'GameDen',
 }
 </script>
 <style></style>
--- pages/shop.vue
+++ pages/shop.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 export default {
-  name: 'shop',
+  name: 'Shop',
 }
 </script>
 <style></style>
