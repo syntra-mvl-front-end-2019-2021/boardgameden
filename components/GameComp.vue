524bc033a6eb757481966750b9db74cca64c4604
stephane_mahop <tatosteph@gmail.com>
2021-04-29 21:31:22 +0200

    init

--- components/GameComp.vue
+++ components/GameComp.vue
@@ -44,10 +44,8 @@ export default {
   margin: auto;
   display: grid;
   place-items: center;
-
   .game-img {
     height: 10rem;
-
     box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
       0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
       0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
