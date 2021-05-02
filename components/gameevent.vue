56f44ae1e3624f5b0608c7c43c2a6fea095588f4
stephane_mahop <tatosteph@gmail.com>
2021-05-02 16:56:47 +0200

    comit

---
+++ components/gameevent.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  <h1>
+{{tittle}}
+  </h1>
+     <FormulateForm
+      v-model="formData"
+      @submit="submit"
+    >
+      <FormulateInput
+        type="text"
+        name="user_name"
+        label="Username"
+        validation="required|alphanumeric"
+      />
+      <FormulateInput
+        type="text"
+        name="tittle"
+        label="tittle"
+        validation-name="tittle"
+        validation="required"
+      />
+      <FormulateInput type="hidden" name="role" />
+      <FormulateErrors />
+      <FormulateInput type="submit" name="Submit" />
+    </FormulateForm>
+    </div>
+</template>
+<script>
+export default {
+  name: 'gameevent',
+    data() {
+    return {
+      formErrors: [],
+      formData: {
+        user_name: '',
+        first_name: '',
+        tittle: '',
+    
+      },
+    }
+  },
+}
+</script>
+
--- pages/gameDen.vue
+++ pages/gameDen.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <h2>Play!</h2>
+    <HomePageSearch />
   </div>
 </template>
 <script>
+import HomePageSearch from '../components/HomePageSearch.vue'
+import gameevent from '../components/gameevent.vue'
 export default {
   name: 'GameDen',
 }
