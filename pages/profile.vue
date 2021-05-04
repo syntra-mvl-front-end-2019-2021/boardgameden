9f9dda54174823b90975856954351583233a4472
Ruben <rubenquaegebeur@gmail.Com>
2021-05-04 08:47:51 +0200

    Profile

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <h1>Profile</h1>
+    <h1>Profile123</h1>
     <ul v-if="userData">
       <li>email: {{ userData.email }}</li>
     </ul>
-
     <Collection />
+    <h3>test</h3>
+    <h2>{{ game.name }}</h2>
   </div>
 </template>
+
 <script>
+
 export default {
   name: 'ProfilePage',
 
