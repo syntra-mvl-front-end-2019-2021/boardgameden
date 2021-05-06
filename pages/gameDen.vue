6ec54497a998978c9a9d1d1dfb4da293f0ec3b29
stephane_mahop <tatosteph@gmail.com>
2021-05-06 19:13:14 +0200

    init

--- pages/gameDen.vue
+++ pages/gameDen.vue
@@ -1,15 +1,66 @@
 <template>
   <div>
     <h2>Play!</h2>
-    <HomePageSearch />
-    <gameevent />
+    <h1>
+      {{ tittle }}
+    </h1>
+    <button @click="toggle = !toggle">created event</button>
+    <FormulateForm v-if="toggle" v-model="formData" @submit="submit">
+      <FormulateInput
+        type="text"
+        name="Event"
+        label="Event"
+        validation-name="Event"
+        validation="required"
+      />
+      <FormulateInput
+        type="text"
+        name="Boardgame"
+        label="Boardgame"
+        validation-name="Boardgame"
+      
+      />
+      <FormulateInput
+        type="date"
+        name="Date"
+        label="Date"
+        :validation="'required|before:' + minDate"
+      />
+      <FormulateInput
+        type="text"
+        name="Location"
+        label="Location"
+        validation="required|alphanumeric"
+      />
+      <FormulateInput
+        type="text"
+        name="User"
+        label="User"
+        validation="required|alphanumeric"
+      />
+      <FormulateInput type="hidden" name="role" />
+      <FormulateErrors />
+      <FormulateInput type="submit" name="Submit" />
+    </FormulateForm>
   </div>
 </template>
 <script>
-import HomePageSearch from '../components/HomePageSearch.vue'
-import gameevent from '../components/gameevent.vue'
 export default {
-  name: 'GameDen',
+  name: 'Gameevent',
+  data() {
+    return {
+      formErrors: [],
+      formData: {
+        User: '',
+        tittle: '',
+        Date: '',
+        Location: '',
+        Event: '',
+        Boardgame: '',
+      },
+      toggle: false,
+    }
+  },
+  methods: {},
 }
 </script>
-<style></style>
