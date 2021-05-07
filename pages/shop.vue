5b50c1ab1e1f1472aa56b3309acbba914a2f04ff
Dmitriy Kozlov <dimitry_kozlov@hotmail.com>
2021-05-07 09:59:15 +0200

    adding axios items boardgame

--- pages/shop.vue
+++ pages/shop.vue
@@ -4,8 +4,27 @@
   </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
   name: 'Shop',
+
+  data() {
+    return {
+      shopItems: [],
+    }
+  },
+  created() {
+    axios
+      .get('http://206.81.26.160/items/boardgames')
+      .then((response) => {
+        this.shopItems = response.data.data
+        console.log(this.shopItems)
+      })
+      // eslint-disable-next-line node/handle-callback-err
+      .catch((error) => {
+        console.log(error.message)
+      })
+  },
 }
 </script>
 <style></style>
