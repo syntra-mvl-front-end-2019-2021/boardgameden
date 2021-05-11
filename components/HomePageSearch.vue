629723245406a7239c331770997c503af3e5ad88
Dmitriy Kozlov <dimitry_kozlov@hotmail.com>
2021-05-11 16:20:19 +0200

    adding structure to func search

--- components/HomePageSearch.vue
+++ components/HomePageSearch.vue
@@ -1,39 +1,57 @@
 <template>
   <section class="s-search">
-    <form class="form">
+    <form class="form" @submit.prevent="HandleSearch">
       <div class="form__search">
         <label for="search"></label>
         <input
           id="search"
+          v-model="search_query"
           type="text"
           placeholder="Zoek je avontuur"
           name="search"
+          required
         />
       </div>
-      <div class="form__city">
+      <!-- <div class="form__city">
         <label for="city"></label>
         <input id="city" type="text" placeholder="City" name="city" />
-      </div>
-      <div class="form__radius">
-        <label for="radius"></label>
-        <select id="radius" required name="radius">
+      </div> -->
+      <!-- <div class="form__radius"> -->
+      <!-- <label for="radius"></label> -->
+      <!-- <select id="radius" required name="radius">
           <option value="" disabled selected>Radius</option>
 
           <option value="10">10</option>
           <option value="20">20</option>
           <option value="30">30</option>
           <option value="40">40</option>
-        </select>
-      </div>
-      <button type="submit">Fetch</button>
+        </select> -->
+      <!-- </div> -->
+      <button type="submit">Search</button>
     </form>
   </section>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   setup() {
-    return {}
+    const searchQuery = ref('')
+    const gameList = ref([])
+    const HandleSearch = async () => {
+      gameList.value = await fetch(
+        ` https://www.boardgameatlas.com/api/search?name=${searchQuery.value}client_id=KrUdcULOvp`
+      )
+        .then((res) => res.json())
+        .then((data) => data.results)
+      searchQuery.value = ''
+    }
+    return {
+      searchQuery,
+      gameList,
+      HandleSearch,
+    }
   },
 }
 </script>
--- pages/shop.vue
+++ pages/shop.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <h2>Shop</h2>
+    <div class="c-shopItems">
+      <div
+        v-for="item in shopItems"
+        :key="item.bg_atlas_id"
+        class="c-shopItems_item"
+      >
+        {{ item.bg_atlas_id }}
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -11,11 +20,12 @@ export default {
   data() {
     return {
       shopItems: [],
+      baseURL: process.env.BASE_URL,
     }
   },
   created() {
     axios
-      .get('http://206.81.26.160/items/boardgames')
+      .get(this.baseURL + '/items/boardgames')
       .then((response) => {
         this.shopItems = response.data.data
         console.log(this.shopItems)
@@ -27,4 +37,14 @@ export default {
   },
 }
 </script>
-<style></style>
+<style lang="scss">
+.c-shopItems {
+  height: 100vh;
+  width: 100%;
+  &_item {
+    width: 100px;
+    height: 100px;
+    background: $orange;
+  }
+}
+</style>
