ddd6135fa4abb9b0cee24a514b2426c92b39a4c8
philip franssen <philipfranssen@hotmail.com>
2021-05-16 18:58:20 +0200

    collection profile page

--- components/CollectionItem.vue
+++ components/CollectionItem.vue
@@ -1,18 +1,23 @@
 <template>
-  <div>
+  <!-- <div>
     <strong>{{ game.bg_atlas_id }}</strong>
+  </div> -->
+  <div v-if="game">
+    <img class="game-img" :src="game.thumb_url" alt="game.name" />
+    <p class="game-title">{{ game.gbId }}</p>
+    <h3 class="game-title">{{ game.name }}</h3>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'CollectionItem',
   props: {
     game: {
       type: Object,
       required: true,
     },
   },
-
   created() {
     this.$axios
       .get(
