bc99b07d844a322a23710cbde635e2e0f452b139
philip franssen <philipfranssen@hotmail.com>
2021-06-16 23:48:40 +0200

    styling

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,64 +1,66 @@
 <template>
-  <section class="c-contactPage">
-    <Logo />
-    <div class="c-contactPage__form">
-      <h1>Contact</h1>
-      <form @submit.prevent="createQuestion" @submit="resetForm">
-        <div class="round-input-names">
+  <div class="bg-col">
+    <section class="c-contactPage">
+      <div class="c-contactPage__form">
+        <h1>Contact</h1>
+        <form @submit.prevent="createQuestion" @submit="resetForm">
+          <div class="round-input-names">
+            <div class="round-input">
+              <div class="round-input-container">
+                <div class="round-input-decorator">
+                  <div class="round-input-border-left"></div>
+                  <label class="round-input-label-text"> First Name </label>
+                  <div class="round-input-border-right"></div>
+                </div>
+                <input v-model="firstname" type="text" />
+              </div>
+            </div>
+            <div class="round-input">
+              <div class="round-input-container">
+                <div class="round-input-decorator">
+                  <div class="round-input-border-left"></div>
+                  <label class="round-input-label-text"> Last Name </label>
+                  <div class="round-input-border-right"></div>
+                </div>
+                <input v-model="lastname" type="text" />
+              </div>
+            </div>
+          </div>
           <div class="round-input">
             <div class="round-input-container">
               <div class="round-input-decorator">
                 <div class="round-input-border-left"></div>
-                <label class="round-input-label-text"> First Name </label>
+                <label class="round-input-label-text"> email </label>
                 <div class="round-input-border-right"></div>
               </div>
-              <input v-model="firstname" type="text" />
+              <input v-model="email" type="text" />
             </div>
           </div>
-          <div class="round-input">
+          <div class="round-textarea">
             <div class="round-input-container">
               <div class="round-input-decorator">
                 <div class="round-input-border-left"></div>
-                <label class="round-input-label-text"> Last Name </label>
+                <label class="round-input-label-text"> Your question </label>
                 <div class="round-input-border-right"></div>
               </div>
-              <input v-model="lastname" type="text" />
+              <textarea v-model="question" placeholder=""></textarea>
             </div>
           </div>
-        </div>
-        <div class="round-input">
-          <div class="round-input-container">
-            <div class="round-input-decorator">
-              <div class="round-input-border-left"></div>
-              <label class="round-input-label-text"> email </label>
-              <div class="round-input-border-right"></div>
-            </div>
-            <input v-model="email" type="text" />
-          </div>
-        </div>
-        <div class="round-textarea">
-          <div class="round-input-container">
-            <div class="round-input-decorator">
-              <div class="round-input-border-left"></div>
-              <label class="round-input-label-text"> Your question </label>
-              <div class="round-input-border-right"></div>
-            </div>
-            <textarea v-model="question" placeholder=""></textarea>
-          </div>
-        </div>
 
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
-  </section>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+      <!-- <Logo class="logo" /> -->
+    </section>
+  </div>
 </template>
 <script>
-import Logo from '~/components/Logo'
+// import Logo from '~/components/Logo'
 export default {
   name: 'Faqquestion',
-  components: { Logo },
+  // components: { Logo },
 
   data() {
     return {
@@ -106,29 +108,34 @@ export default {
 </script>
 
 <style lang="scss">
+.bg-col {
+  // background: linear-gradient(rgba(129, 203, 235, 0.767), #ffffff 100%);
+  background-color: rgba($bluegreen, 0.8);
+  transform: skewY(-20deg);
+  margin-bottom: 18rem;
+}
 .c-contactPage {
-  @include flexCenter();
-  max-width: 100%;
-  height: 100vh;
+  // @include flexCenter();
+  display: flex;
+  flex-direction: column;
+  width: 60%;
   margin: auto;
-  background: linear-gradient(rgba(129, 203, 235, 0.767), #ffffff 100%);
 
   &__form {
     @include flexCenter();
     flex-direction: column;
-    height: 100%;
     text-align: center;
     flex: 1;
     width: 100%;
     max-width: 1200px;
+    transform: skewY(20deg);
 
     h1 {
       color: #ea5d30;
     }
     form {
-      height: 75%;
-      margin-left: auto;
-      width: 75%;
+      width: 90%;
+      margin: auto;
       @include flexCenter();
       flex-direction: column;
       justify-content: space-evenly;
@@ -144,13 +151,11 @@ export default {
         width: 100%;
       }
       .round-input-container {
-        @include flexCenter();
         display: inline-block;
         position: relative;
         margin: 10px 0;
         flex-direction: column;
         width: 100%;
-        color: rgba(129, 203, 235);
         .round-input-decorator {
           position: absolute;
           top: 0;
@@ -163,7 +168,7 @@ export default {
 
         .round-input-label-text {
           font-size: 0.8em;
-          color: rgb(98, 196, 238);
+          color: white;
           padding: 0 4px;
           transform: translate(0, -8px);
           white-space: nowrap;
@@ -174,13 +179,13 @@ export default {
           box-sizing: border-box;
           height: 100%;
           border-radius: 10px 0 0 10px;
-          border: 1px solid #ea5d30;
+          border: 1px solid white;
           border-right: none;
         }
         .round-input-border-right {
           flex: 1 0 50px;
           border-radius: 0 10px 10px 0;
-          border: 1px solid #ea5d30;
+          border: 1px solid white;
           border-left: none;
         }
 
@@ -190,11 +195,11 @@ export default {
           position: relative;
           font-size: 1em;
           padding: 13px 20px 13px 15px;
-          min-width: 250px;
+          min-width: 200px;
           border: none;
           background: none;
           border-radius: 10px;
-          border-bottom: 1px solid #ea5d30;
+          border-bottom: 1px solid white;
           outline: none;
           &:focus {
             box-shadow: 1px 1px 3px 0 #ea5f3094;
@@ -211,7 +216,6 @@ export default {
         margin: 10px 0;
         flex-direction: column;
         width: 100%;
-        color: rgba(129, 203, 235);
         .round-input-decorator {
           position: absolute;
           top: 0;
@@ -224,7 +228,7 @@ export default {
 
         .round-input-label-text {
           font-size: 0.8em;
-          color: rgb(98, 196, 238);
+          color: white;
           padding: 0 4px;
           transform: translate(0, -8px);
           white-space: nowrap;
@@ -235,13 +239,13 @@ export default {
           box-sizing: border-box;
           height: 100%;
           border-radius: 10px 0 0 10px;
-          border: 1px solid #ea5d30;
+          border: 1px solid white;
           border-right: none;
         }
         .round-input-border-right {
           flex: 1 0 50px;
           border-radius: 0 10px 10px 0;
-          border: 1px solid #ea5d30;
+          border: 1px solid white;
           border-left: none;
         }
         textarea {
@@ -250,11 +254,11 @@ export default {
           position: relative;
           font-size: 1em;
           padding: 13px 20px 13px 15px;
-          min-width: 250px;
+          min-width: 200px;
           border: none;
           background: none;
           border-radius: 10px;
-          border-bottom: 1px solid #ea5d30;
+          border-bottom: 1px solid white;
           outline: none;
           display: flex;
         }
@@ -262,39 +266,50 @@ export default {
     }
 
     button {
-      background: rgba(129, 203, 235);
-      padding: 0.5rem 1rem;
+      background-color: white;
+      color: $orange;
+      font-weight: 700;
+      font-size: 1rem;
+      padding: 1rem 3rem;
       border: none;
-      border-radius: 8px;
+      border-radius: 10px;
       outline: none;
       cursor: pointer;
       &:focus {
         box-shadow: 1px 1px 3px 0 rgba(129, 203, 235);
       }
+
+      &:hover {
+        background-color: $orange;
+        color: white;
+      }
     }
   }
-  .bigBlueBackground {
-    position: absolute;
-    width: 700px;
-    height: 700px;
-    left: -5rem;
-    top: -25rem;
-    transform: rotate(135deg);
-    .blue {
-      fill: rgba(129, 203, 235);
-      width: 500px;
-      height: 500px;
+}
+
+@media screen and (max-width: $large) {
+  .round-input-names {
+    display: flex;
+    flex-direction: column;
+    .round-input {
+      width: 100% !important;
+      min-width: 200px;
     }
   }
-  .bigOrangeBackground {
-    position: absolute;
-    top: -20rem;
-    right: 15rem;
-    width: 500px;
-    height: 500px;
-    transform: rotate(-135deg);
-    .orange {
-      fill: #ea5b30be;
+
+  .round-input-decorator {
+    min-width: 200px;
+  }
+
+  .round-input-container {
+    min-width: 200px;
+  }
+
+  .c-contactPage__form {
+    form {
+      justify-content: center;
+      align-items: center;
+      text-align: center;
     }
   }
 }
